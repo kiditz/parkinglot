2392211958c9e5a7add045039cce8a19d92cae14
kiditz <kiditzbastara@gmail.com>
2020-07-06 23:19:51 +0700

    Handle method to parse create_parking_lot

--- src/main/java/com/emeriocorp/parkinglot/ParkingLot.java
+++ src/main/java/com/emeriocorp/parkinglot/ParkingLot.java
@@ -1,4 +1,32 @@
 package com.emeriocorp.parkinglot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingLot {
+    // Max lot to handle the maximum value of slot
+    private int maxLot = 0;
+    // Remaining slot to handle sequential number generated in created parking slot
+    private final List<Integer> remainingSlot = new ArrayList<>();
+
+    public void createParkingLot(String numOfLot) {
+        try {
+            setMaxLot(Integer.parseInt(numOfLot));
+            for (int i = 1; i <= getMaxLot(); i++) {
+                remainingSlot.add(i);
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid lot number");
+            System.err.println();
+        }
+    }
+
+
+    public int getMaxLot() {
+        return maxLot;
+    }
+
+    public void setMaxLot(int maxLot) {
+        this.maxLot = maxLot;
+    }
 }
