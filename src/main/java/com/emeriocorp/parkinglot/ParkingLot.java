d2b0ca4c12d7d596a1c13897d684df0436eb77b6
kiditz <kiditzbastara@gmail.com>
2020-07-07 00:02:37 +0700

    Implement method park after created test

--- src/main/java/com/emeriocorp/parkinglot/ParkingLot.java
+++ src/main/java/com/emeriocorp/parkinglot/ParkingLot.java
@@ -1,7 +1,6 @@
 package com.emeriocorp.parkinglot;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ParkingLot {
     // Max lot to handle the maximum value of slot
@@ -9,6 +8,7 @@ public class ParkingLot {
     // Remaining slot to handle sequential number generated in created parking slot
     private final List<Integer> remainingSlot = new ArrayList<>();
 
+    private final Map<Integer, String> slotReqNoMap = new HashMap<>();
     /**
      * Handle command create_parking_lot with input numOfLot by parsing string into integer
      *
@@ -17,6 +17,7 @@ public class ParkingLot {
     public void createParkingLot(String numOfLot) {
         try {
             setMaxLot(Integer.parseInt(numOfLot));
+            remainingSlot.clear();
             for (int i = 1; i <= getMaxLot(); i++) {
                 remainingSlot.add(i);
             }
@@ -24,6 +25,24 @@ public class ParkingLot {
             System.err.println("Invalid lot number");
             System.err.println();
         }
+    }
+
+    public void park(String regNo){
+        if(maxLot == 0){
+            System.out.println("Sorry, parking lot isn't created");
+            return;
+        }
+        if(slotReqNoMap.size() == maxLot){
+            System.out.println("Sorry, parking lot is full");
+            return;
+        }
+
+        // Sorting slot ascending order, so when this method is called, the value is sorted
+        Collections.sort(remainingSlot);
+        String slotNo  = remainingSlot.get(0).toString();
+        slotReqNoMap.put(Integer.parseInt(slotNo), regNo);
+        System.out.println(String.format("Allocated slot number: %s", slotNo));
+        remainingSlot.remove(0);
     }
 
 
@@ -39,5 +58,7 @@ public class ParkingLot {
         return remainingSlot;
     }
 
-
+    public Map<Integer, String> getSlotReqNoMap() {
+        return slotReqNoMap;
+    }
 }
